324654aaaafac04135009c47cd3232bb9adf17a8
Christian Kauhaus <kc@flyingcircus.io>
2019-06-17 11:19:36 +0200

    Override version for dev builds

--- default.nix
+++ default.nix
@@ -7,7 +7,9 @@
 (pkgs.callPackage "${nixpkgs}/pkgs/tools/security/vulnix" {
   pythonPackages = pkgs.python3Packages;
 }).overrideAttrs (
-  old: {
+  old: rec {
     src = lib.cleanSource ./.;
+    version = lib.removeSuffix "\n" (builtins.readFile ./VERSION);
+    name = "vulnix-${version}";
   }
 )
